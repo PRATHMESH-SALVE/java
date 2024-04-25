3d70f2afc9d633a65f6f2a60c97cb07bfb1101f8
Prathmesh Salve <168015208+PRATHMESH-SALVE@users.noreply.github.com>
2024-04-25 20:15:19 +0530

    Create 25apr.java

---
+++ 25apr.java
@@ -0,0 +1,12 @@
+// if else
+class HelloWorld {
+    public static void main(String[] args) {
+       int n =45;
+       int result =0;
+       if(n%2==0)
+       result =5555;
+       else
+       result =54;
+        System.out.println(result);
+    }
+}
